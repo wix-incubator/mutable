ac9478ad92baeb1caa6894ded0334e097bc685a1
Ido Rosenthal <idoro@wix.com>
2015-03-03 21:42:43 +0200

    added test drivers with first driver aDataTypeWithSpec + refactored all remaining require to import, renamed Typeorama to Typorama

--- test/Array.js
+++ test/Array.js
@@ -1,45 +1,33 @@
-let Typeorama = require('../');
-let chai = require('chai');
-let expect = chai.expect;
+import Typorama from "../";
+import {aDataTypeWithSpec} from "./testDrivers/index";
+import {expect} from "chai";
 
 describe('Array data', function() {
 
-    var UserType = Typeorama.define('User', {
-        spec: function(UserType) {
-            return {
-                name: Typeorama.String.withDefault(''),
-                age: Typeorama.Number.withDefault(10)
-            };
-        }
-    });
+    var UserType = aDataTypeWithSpec({
+        name: Typorama.String.withDefault('leon'),
+        age: Typorama.Number.withDefault(10)
+    }, 'User');
 
-    var AddressType = Typeorama.define('Address', {
-        spec: function(AddressType) {
-            return {
-                address: Typeorama.String.withDefault(''),
-                code: Typeorama.Number.withDefault(10)
-            };
-        }
-    });
+    var AddressType = aDataTypeWithSpec({
+        address: Typorama.String.withDefault(''),
+        code: Typorama.Number.withDefault(10)
+    }, 'Address');
 
-    var UserWithAddressType = Typeorama.define('UserWithAddress', {
-        spec: function(UserWithAddressType) {
-            return {
-                user: UserType,
-                address: AddressType
-            };
-        }
-    });
+    var UserWithAddressType = aDataTypeWithSpec({
+        user: UserType,
+        address: AddressType
+    }, 'UserWithAddress');
 
     describe('(Mutable) instance', () => {
 
         it('Should have default length', () => {
-            var numberList = new Typeorama.Array([1,2,3,4], false, Typeorama.Number);
+            var numberList = new Typorama.Array([1,2,3,4], false, Typorama.Number);
             expect(numberList.length).to.equal(4);
         });
 
         it('Should be created once for each data instance', () => {
-            var numberList = new Typeorama.Array([1,2,3,4], false, Typeorama.Number);
+            var numberList = new Typorama.Array([1,2,3,4], false, Typorama.Number);
             var numberListReadOnly = numberList.$asReadOnly();
             var numberListReadOnly2 = numberList.$asReadOnly();
 
@@ -48,28 +36,28 @@ describe('Array data', function() {
 
         describe('at()', () => {
 
-            it('Should return a number for native immutable Typeorama.Number', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+            it('Should return a number for native immutable Typorama.Number', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                 expect(numberList.at(0)).to.equal(1);
             });
 
-            it('Should return a string for native immutable Typeorama.String', () => {
-                var arr = Typeorama.Array.of(Typeorama.String).create(['123','sdfs']);
+            it('Should return a string for native immutable Typorama.String', () => {
+                var arr = Typorama.Array.of(Typorama.String).create(['123','sdfs']);
                 expect(arr.at(0)).to.equal('123');
             });
 
             it('Should return wrapped item that passes the test() of their type', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                 expect(numberList.__options__.subTypes.test(numberList.at(0))).to.equal(true);
             });
 
             it('Should return a typed item for none immutable data (like custom types)', () => {
-                var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                 expect(arr.at(0) instanceof UserType).to.equal(true);
             });
 
             it('Should always return a the same reference for wrapper', () => {
-                var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                 expect(arr.at(0)).to.equal(arr.at(0));
             });
 
@@ -78,13 +66,13 @@ describe('Array data', function() {
                     {_type:'UserType',  name: 'avi', age: 12},
                     {_type:'AddressType', name: 'avi', age: 12}
                 ];
-                var arr = Typeorama.Array.of({UserType: UserType, AddressType: AddressType}).create(data);
+                var arr = Typorama.Array.of({UserType: UserType, AddressType: AddressType}).create(data);
                 expect(arr.at(0) instanceof UserType).to.equal(true);
                 expect(arr.at(1) instanceof AddressType).to.equal(true);
             });
 
             it('Should modify inner complex data', () => {
-                var arrComplexType = Typeorama.Array.of(UserWithAddressType).create([{}, {}, {}]);
+                var arrComplexType = Typorama.Array.of(UserWithAddressType).create([{}, {}, {}]);
 
                 arrComplexType.at(1).user.name = 'modified user name';
 
@@ -92,12 +80,12 @@ describe('Array data', function() {
             });
 
             it('Should handle multi level array', () => {
-                var arrComplexType = Typeorama.Array.of(Typeorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]]);
+                var arrComplexType = Typorama.Array.of(Typorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]]);
                 expect(arrComplexType.at(0).at(0) instanceof UserWithAddressType).to.equal(true);
             });
 
             it('Should change type form multi level array', () => {
-                var arrComplexType = Typeorama.Array.of(Typeorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]]);
+                var arrComplexType = Typorama.Array.of(Typorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]]);
                 var userWithAddress = arrComplexType.at(0).at(0);
 
                 userWithAddress.user.name = 'you got a new name';
@@ -108,7 +96,7 @@ describe('Array data', function() {
             it('Should keep read only item as read only', () => {
                 var userDefaultName = UserWithAddressType.getFieldsSpec().user.defaults().name;
                 var readOnlyData = new UserWithAddressType().$asReadOnly();
-                var arrComplexType = Typeorama.Array.of(UserWithAddressType).create([readOnlyData]);
+                var arrComplexType = Typorama.Array.of(UserWithAddressType).create([readOnlyData]);
 
                 var readOnlyItemData = arrComplexType.at(0);
 
@@ -122,7 +110,7 @@ describe('Array data', function() {
 
         describe('push',function(){
             it('it should add a number to an array ', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                 var lengthBeforePush = numberList.length;
                 var newIndex = numberList.push(5);
                 expect(newIndex).to.equal(5);
@@ -132,13 +120,13 @@ describe('Array data', function() {
             });
 
             it('Should add a typed item for none immutable data (like custom types)', () => {
-                var arr = Typeorama.Array.of(UserType).create([]);
+                var arr = Typorama.Array.of(UserType).create([]);
                 arr.push({name:'zag'});
                 expect(arr.at(0) instanceof UserType).to.equal(true);
             });
 
             it('Should add a typed item form multiple types if there is _type field', () => {
-                 var arr = Typeorama.Array.of({UserType: UserType, AddressType: AddressType}).create([]);
+                 var arr = Typorama.Array.of({UserType: UserType, AddressType: AddressType}).create([]);
                 arr.push({_type:'UserType'});
                 arr.push({_type:'AddressType'});
                 expect(arr.at(0) instanceof UserType).to.equal(true);
@@ -154,11 +142,11 @@ describe('Array data', function() {
         
         describe('as field on data object', () => {
 
-            var GroupType = Typeorama.define('GroupType', {
+            var GroupType = Typorama.define('GroupType', {
                 spec: function(GroupType) {
                     return {
-                        title: Typeorama.String,
-                        users: Typeorama.Array.of(UserType)
+                        title: Typorama.String,
+                        users: Typorama.Array.of(UserType)
                     };
                 }
             });
@@ -183,29 +171,29 @@ describe('Array data', function() {
     describe('(Read Only) instance', () => {
 
         it('Should have default length', () => {
-            var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]).$asReadOnly();
+            var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]).$asReadOnly();
             expect(numberList.length).to.equal(4);
         });
 
         describe('at()', () => {
 
-            it('Should return a number for native immutable Typeorama.Number', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]).$asReadOnly();
+            it('Should return a number for native immutable Typorama.Number', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]).$asReadOnly();
                 expect(numberList.at(0)).to.equal(1);
             });
 
-            it('Should return a string for native immutable Typeorama.String', () => {
-                var arr = Typeorama.Array.of(Typeorama.String).create(['123','sdfs']).$asReadOnly();
+            it('Should return a string for native immutable Typorama.String', () => {
+                var arr = Typorama.Array.of(Typorama.String).create(['123','sdfs']).$asReadOnly();
                 expect(arr.at(0)).to.equal('123');
             });
 
             it('Should return wrapped item that passes the test() of their type', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]).$asReadOnly();
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]).$asReadOnly();
                 expect(numberList.__options__.subTypes.test(numberList.at(0))).to.equal(true);
             });
 
             it('Should return a typed item for none immutable data (like custom types)', () => {
-                var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]).$asReadOnly();
+                var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]).$asReadOnly();
                 expect(arr.at(0) instanceof UserType).to.equal(true);
             });
 
@@ -214,14 +202,14 @@ describe('Array data', function() {
                     {_type:'UserType',  name: 'avi', age: 12},
                     {_type:'AddressType', name: 'avi', age: 12}
                 ];
-                var arr = Typeorama.Array.of({UserType: UserType, AddressType: AddressType}).create(data).$asReadOnly();
+                var arr = Typorama.Array.of({UserType: UserType, AddressType: AddressType}).create(data).$asReadOnly();
                 expect(arr.at(0) instanceof UserType).to.equal(true);
                 expect(arr.at(1) instanceof AddressType).to.equal(true);
             });
 
             it('Should not modify inner complex data', () => {
                 var userDefaultName = UserWithAddressType.getFieldsSpec().user.defaults().name;
-                var arrComplexType = Typeorama.Array.of(UserWithAddressType).create([{}, {}, {}]).$asReadOnly();
+                var arrComplexType = Typorama.Array.of(UserWithAddressType).create([{}, {}, {}]).$asReadOnly();
 
                 arrComplexType.at(1).user.name = 'modified user name';
 
@@ -229,12 +217,12 @@ describe('Array data', function() {
             });
 
             it('Should handle multi level array', () => {
-                var arrComplexType = Typeorama.Array.of(Typeorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]], true);
+                var arrComplexType = Typorama.Array.of(Typorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]], true);
                 expect(arrComplexType.at(0).at(0) instanceof UserWithAddressType).to.equal(true);
             });
 
             it('Should not change type from multi level array', () => {
-                var arrComplexType = Typeorama.Array.of(Typeorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]], true);
+                var arrComplexType = Typorama.Array.of(Typorama.Array.of(UserWithAddressType)).create([[{}], [{}], [{}]], true);
                 var userWithAddress = arrComplexType.at(0).at(0);
 
                 userWithAddress.user.name = 'you got a new name';
@@ -246,7 +234,7 @@ describe('Array data', function() {
         
         describe('push',function(){
             it('should not modify an array ', () => {
-                var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]).$asReadOnly();
+                var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]).$asReadOnly();
                 var lengthBeforePush = numberList.length;
                 var newIndex = numberList.push(5);
                 expect(newIndex).to.equal(null);
@@ -259,27 +247,27 @@ describe('Array data', function() {
         describe('Type Invalidation',()=>{
             describe('$isInvalidated',() =>{
                 it('Should return false for unmodified data', () => {
-                    var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+                    var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                     expect(numberList.$isInvalidated()).to.equal(false);
                 });
                 xit('Should return true for modified data', () => {
-                    var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+                    var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                     numberList.push(5);
                     expect(numberList.$isInvalidated()).to.equal(true);
                 });
                 it('Should return true for data when a child value has changed', () => {
-                    var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                    var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                     arr.at(0).name = "gaga";
                     expect(arr.$isInvalidated()).to.equal(true);
                 });
                 xit('Should return true for data when a child value has changed after isinvalidates was already called', () => {
-                    var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                    var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                     expect(arr.$isInvalidated()).to.equal(false);
                     arr.at(0).name = "gaga";
                     expect(arr.$isInvalidated()).to.equal(true);
                 });
                 it('Should return false for data when only a parent/sibling value has changed', () => {
-                    var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12},{name: 'shlomo', age: 15}]);
+                    var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12},{name: 'shlomo', age: 15}]);
 
                     arr.at(0).name = "gaga";
                     expect(arr.at(0).$isInvalidated()).to.equal(true);
@@ -290,7 +278,7 @@ describe('Array data', function() {
 
             describe('$revalidate',() =>{
                 xit('Should reset data invalidation', () => {
-                    var numberList = Typeorama.Array.of(Typeorama.Number).create([1,2,3,4]);
+                    var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                     numberList.push(5);
                     expect(numberList.$isInvalidated()).to.equal(true);
                     numberList.$revalidate();
@@ -298,7 +286,7 @@ describe('Array data', function() {
 
                 });
                 it('Should reset deep data invalidation', () => {
-                    var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                    var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                     arr.at(0).name = "gaga";
                     expect(arr.$isInvalidated()).to.equal(true);
                     expect(arr.at(0).$isInvalidated()).to.equal(true);
@@ -311,7 +299,7 @@ describe('Array data', function() {
 
             describe('$resetValidationCheck',() =>{
                 it('it Should allow isInvalidated to return true for data when a child value has changed after isinvalidates was already called', () => {
-                    var arr = Typeorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
+                    var arr = Typorama.Array.of(UserType).create([{name: 'avi', age: 12}]);
                     expect(arr.$isInvalidated()).to.equal(false);
                     expect(arr.at(0).$isInvalidated()).to.equal(false);
                     arr.at(0).name = "gaga";
