7c4b84fee2fcf490c3f94c1c35164724510f41d7
NirBenita <benitanir@gmail.com>
2015-03-12 16:04:45 +0200

    Added several array methods

--- src/array.js
+++ src/array.js
@@ -2,41 +2,49 @@ import _ from "lodash"
 import defineType from "./defineType"
 import BaseType from "./BaseType"
 import number from "./number"
-import {generateWithDefault} from "./defineTypeUtils"
+import {
+    generateWithDefault
+}
+from "./defineTypeUtils"
 
 export default class _Array extends BaseType {
 
-    static defaults(){ return []; }
+    static defaults() {
+        return [];
+    }
 
-    static test(value){ return Array.isArray(value); }
+    static test(value) {
+        return Array.isArray(value);
+    }
 
-    static wrapValue(value, spec, isReadOnly, options){
+    static wrapValue(value, spec, isReadOnly, options) {
         return value.reduce((wrappedList, itemValue) => {
             wrappedList.push(this._wrapSingleItem(itemValue, isReadOnly, options));
             return wrappedList;
         }, []);
     }
 
-    static _wrapSingleItem(itemValue, isReadOnly, options){
-        if(itemValue instanceof BaseType){
+    static _wrapSingleItem(itemValue, isReadOnly, options) {
+        if (itemValue instanceof BaseType) {
             return itemValue;
-        } else if(typeof options.subTypes === 'function'){
+        } else if (typeof options.subTypes === 'function') {
             return options.subTypes.create(itemValue, isReadOnly, options.subTypes.options);
-        } else if(typeof options.subTypes === 'object'){
+        } else if (typeof options.subTypes === 'object') {
             var subType = options.subTypes[itemValue._type];
             return subType.create(itemValue, isReadOnly, subType.options);
         }
     }
 
-    static of(subTypes, defaults, test){
-        return this.withDefault(defaults, test, { subTypes });
+    static of(subTypes, defaults, test) {
+        return this.withDefault(defaults, test, {
+            subTypes
+        });
     };
 
-    constructor(value=[], isReadOnly=false, options={}){
-        if(options.subTypes && _.isArray(options.subTypes))
-        {
+    constructor(value = [], isReadOnly = false, options = {}) {
+        if (options.subTypes && _.isArray(options.subTypes)) {
             var subTypesObj = {};
-            options.subTypes.forEach(function(item){
+            options.subTypes.forEach(function(item) {
                 subTypesObj[item.displayName] = item;
             });
             options.subTypes = subTypesObj;
@@ -44,91 +52,62 @@ export default class _Array extends BaseType {
         BaseType.call(this, value, isReadOnly, options);
     }
 
-    at(index){
-        var item = this.__value__[index];
-        return (this.__isReadOnly__ && item instanceof BaseType) ? item.$asReadOnly() : item;
+    // To check with Nadav: map, pop, push, reverse, shift, sort 
+    // Mutator methods
+
+    copyWithin() {
+
     }
 
-    push(newItem){
-        if(this.__isReadOnly__){
+    fill() {
+
+    }
+
+    pop() {
+       if (this.__isReadOnly__) {
             return null;
         }
-        this.__isInvalidated__= true;
+        this.__isInvalidated__ = true;
+        return this.__value__.pop();
+    }
+
+    push(newItem) {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        this.__isInvalidated__ = true;
         return this.__value__.push(this.constructor._wrapSingleItem(newItem, this.__isReadOnly__, this.__options__));
     }
-    forEach(cb){
-        var that = this;
-        this.__value__.forEach(function(item,index,arr){
-            cb(item,index,that);
-        });
-    }
 
-    splice(index,removeCount, ...addedItems){
-        if(this.__isReadOnly__){
+    reverse() {
+       if (this.__isReadOnly__) {
             return null;
         }
-        this.__isInvalidated__= true;
-        var spliceParams = [index,removeCount];
-        addedItems.forEach(function(newItem){
-           spliceParams.push(this.constructor._wrapSingleItem(newItem, this.__isReadOnly__, this.__options__))
-        }.bind(this));
-        return this.__value__.splice.apply(this.__value__,spliceParams);
-        //return this.__value__.push(this.constructor._wrapSingleItem(newItem, this.__isReadOnly__, this.__options__));
+        this.__isInvalidated__ = true;
+        return this.__value__.reverse();
     }
 
-    concat(...addedArrays){
-        var res = new this.constructor(this.__value__,false,this.__options__);
-        addedArrays.forEach(function(arr){
-            arr.forEach(function(item){
-                res.push(item);
-            })
-        });
-        return res;
+    shift() {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        this.__isInvalidated__ = true;
+        return this.__value__.shift();
     }
 
-    every(cb){
-        var self = this;
-        return this.__value__.every(function(element, index, array) {
-            return cb(element, index, self)
-        });
+    sort(cb) {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        this.__isInvalidated__ = true;
+        return this.__value__.sort(cb);
     }
 
-    some(cb){
-        var self = this;
-        return this.__value__.some(function(element, index, array){
-            return cb(element, index, self);
-        });
-    }
-
-    find(cb){
-        var self = this;
-        return _.find(this.__value__, function(element, index, array) {
-            return cb(element, index, self);
-        });
-        return _.find(this.__value__, cb);
-    }
-
-    findIndex(cb){
-        var self = this;
-        return _.findIndex(this.__value__, function (element, index, array) {
-            return cb(element, index, self)
-        })
-        return _.findIndex(this.__value__, cb);
-    }
-
-    filter(cb){
-        var self = this;
-        var filteredArray = this.__value__.filter(function(element, index, array) {
-            return cb(element, index, self);
-        });
-        return new this.constructor(filteredArray, false, this.__options__);
-    }
-
-    setValue(newValue){
-        if(newValue instanceof _Array){
+    setValue(newValue) {
+        if (newValue instanceof _Array) {
             newValue = newValue.toJSON();
         }
-        if(_.isArray(newValue)){
+        if (_.isArray(newValue)) {
             this.__value__ = [];
             _.forEach(newValue, (itemValue) => {
                 this.push(itemValue);
@@ -136,44 +115,193 @@ export default class _Array extends BaseType {
         }
     }
 
-    $asReadOnly(){
-        if(!this.__readOnlyInstance__) {
+    splice(index, removeCount, ...addedItems) {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        this.__isInvalidated__ = true;
+        var spliceParams = [index, removeCount];
+        addedItems.forEach(function(newItem) {
+            spliceParams.push(this.constructor._wrapSingleItem(newItem, this.__isReadOnly__, this.__options__))
+        }.bind(this));
+        return this.__value__.splice.apply(this.__value__, spliceParams);
+        //return this.__value__.push(this.constructor._wrapSingleItem(newItem, this.__isReadOnly__, this.__options__));
+    }
+
+    unshift() {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        this.__isInvalidated__ = true;
+        return this.__value__.unshift();
+    }
+
+    // Accessor methods
+    at(index) {
+        var item = this.__value__[index];
+        return (this.__isReadOnly__ && item instanceof BaseType) ? item.$asReadOnly() : item;
+    }
+
+    concat(...addedArrays) {
+        var res = new this.constructor(this.__value__, false, this.__options__);
+        addedArrays.forEach(function(arr) {
+            arr.forEach(function(item) {
+                res.push(item);
+            })
+        });
+        return res;
+    }
+
+    includes() {
+
+    }
+
+    join() {
+
+    }
+
+    slice() {
+
+    }
+
+    toSource() {
+
+    }
+
+    toString() {
+
+    }
+
+    toLocaleString() {
+
+    }
+
+    indexOf() {
+
+    }
+
+    lastIndexOf() {
+
+    }
+
+    // Iteration methods        
+
+    forEach(cb) {
+        var that = this;
+        this.__value__.forEach(function(item, index, arr) {
+            cb(item, index, that);
+        });
+    }
+
+    entries() {
+
+    }
+
+    every(cb) {
+        var self = this;
+        return this.__value__e__.every(function(element, index, array) {
+            return cb(element, index, self)
+        });
+    }
+
+    some(cb) {
+        var self = this;
+        return this.__value__.some(function(element, index, array) {
+            return cb(element, index, self);
+        });
+    }
+
+    filter(cb) {
+        var self = this;
+        var filteredArray = this.__value__.filter(function(element, index, array) {
+            return cb(element, index, self);
+        });
+        return new this.constructor(filteredArray, false, this.__options__);
+    }
+
+    find(cb) {
+        var self = this;
+        return _.find(this.__value__, function(element, index, array) {
+            return cb(element, index, self);
+        });
+        return _.find(this.__value__, cb);
+    }
+
+    findIndex(cb) {
+        var self = this;
+        return _.findIndex(this.__value__, function(element, index, array) {
+            return cb(element, index, self)
+        })
+        return _.findIndex(this.__value__, cb);
+    }
+
+    keys() {
+
+    }
+
+    // Seeing as 'map' simply reads an array, and then duplicates it. Can I map a readOnly array?
+    map(cb, ctx) {
+        if (this.__isReadOnly__) {
+            return null;
+        }
+        // this.__isInvalidated__= true;
+
+        var arr = this.__value__.map(cb, ctx || this);
+        return new this.constructor(arr, false, this.__options__)
+    }
+
+    reduce() {
+
+    }
+
+    reduceRight() {
+
+    }
+
+    values() {
+
+    }
+
+
+
+
+
+    $asReadOnly() {
+        if (!this.__readOnlyInstance__) {
             this.__readOnlyInstance__ = this.constructor.type.create(this.__value__, true, this.__options__);
         }
         return this.__readOnlyInstance__;
     }
 
-    $isInvalidated(){
-        if(this.__isInvalidated__==-1) {
-            var invalidatedField = _.find(this.__value__, (item, index)=>{
-                if(item instanceof BaseType)
-                {
+    $isInvalidated() {
+        if (this.__isInvalidated__ == -1) {
+            var invalidatedField = _.find(this.__value__, (item, index) => {
+                if (item instanceof BaseType) {
                     return item.$isInvalidated();
                 }
             });
-            if(invalidatedField) {
+            if (invalidatedField) {
                 this.__isInvalidated__ = true;
-            }else{
+            } else {
                 this.__isInvalidated__ = false;
             }
         }
         return this.__isInvalidated__;
     }
 
-    $revalidate(){
+    $revalidate() {
         this.__isInvalidated__ = -1;
-        _.forEach(this.__value__, (item, index)=>{
-            if(item instanceof BaseType)
-            {
+        _.forEach(this.__value__, (item, index) => {
+            if (item instanceof BaseType) {
                 item.$revalidate();
             }
         });
     }
 
-    $resetValidationCheck(){
+    $resetValidationCheck() {
         this.__isInvalidated__ = this.__isInvalidated__ || -1;
-        _.forEach(this.__value__, (item, index)=>{
-            if(item instanceof BaseType) {
+        _.forEach(this.__value__, (item, index) => {
+            if (item instanceof BaseType) {
                 item.$resetValidationCheck();
             }
         });
@@ -183,8 +311,8 @@ export default class _Array extends BaseType {
 
 _Array.withDefault = generateWithDefault();
 
-defineType('Array',{
-    spec: function(){
+defineType('Array', {
+    spec: function() {
         return {
             length: number.withDefault(0)
         };
--- test/Array.js
+++ test/Array.js
@@ -44,7 +44,7 @@ describe('Array data', function() {
             it('Should return a string for native immutable Typorama.String', () => {
                 var arr = Typorama.Array.of(Typorama.String).create(['123','sdfs']);
                 expect(arr.at(0)).to.equal('123');
-            });
+            })
 
             it('Should return wrapped item that passes the test() of their type', () => {
                 var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
@@ -107,9 +107,66 @@ describe('Array data', function() {
             });
 
         });
+        
+        describe('pop()', function() {
+            it('should remove the last element from an array', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([1, 2, 3, 4]);
+                var lengthBeforePop = numberList.length;
+                var valueRemoved = numberList.pop();
+
+                expect(numberList.length).to.equal(lengthBeforePop - 1);
+                expect(valueRemoved).to.equal(4);
+                expect(numberList.$isInvalidated()).to.equal(true);
+            });
+
+            it('should return undefined if called on an empty array', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([]);
+                expect(numberList.pop()).to.be.undefined;
+
+            });
+        });
+
+        describe('reverse()', function() {
+            it('should reverse the order of elements in an array', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([1, 2, 3, 4]);
+                var newList = numberList.concat();
+                newList.reverse();
+                expect(numberList.length).to.equal(newList.length);
+                for (var i = 0; i < numberList.length; i++) {
+                    expect(numberList.at(i)).to.equal(newList.at(newList.length - i - 1));
+                };
+                expect(newList.$isInvalidated()).to.equal(true);
+            });
+        });
+        describe('sort()', function() {
+            it('should sort the elements of an anarray in place, and returns the array', () => {
+                var stringArray = Typorama.Array.of(Typorama.String).create(['Blue', 'Humpback', 'Beluga']);
+                var numberArray = Typorama.Array.of(Typorama.Number).create([40, 1, 5, 200]);
+
+                function compareNumbers(a, b) {
+                    return a - b;
+                }
+                expect(stringArray.sort()).to.eql(['Beluga','Blue','Humpback']);
+                expect(numberArray.sort()).to.eql([1, 200, 40, 5]);
+                expect(numberArray.sort(compareNumbers)).to.eql([1, 5, 40, 200]);
+                expect(stringArray.$isInvalidated()).to.equal(true);
+            });
+        });
+        describe('shift()', function(){
+            it('should remove the first element from an array, and returns that element', () => {
+                var numberList = Typorama.Array.of(Typorama.Number).create([1, 2, 3, 4]);
+                var lengthBeforePop = numberList.length;
+                var arrayBeforeShift = numberList.concat();
+                var valueRemoved = numberList.shift();
+
+                expect(arrayBeforeShift.at(0)).to.equal(valueRemoved);
+                expect(numberList.length).to.equal(lengthBeforePop - 1);
+                expect(numberList.$isInvalidated()).to.equal(true);
+            });
+        });
 
         describe('push()',function(){
-            it('it should add a number to an array ', () => {
+            it('should add a number to an array ', () => {
                 var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                 var lengthBeforePush = numberList.length;
                 var newIndex = numberList.push(5);
@@ -119,13 +176,13 @@ describe('Array data', function() {
                 expect(numberList.$isInvalidated()).to.equal(true);
             });
 
-            it('Should add a typed item for none immutable data (like custom types)', () => {
+            it('should add a typed item for none immutable data (like custom types)', () => {
                 var arr = Typorama.Array.of(UserType).create([]);
                 arr.push({name:'zag'});
                 expect(arr.at(0) instanceof UserType).to.equal(true);
             });
 
-            it('Should add a typed item form multiple types if there is _type field', () => {
+            it('should add a typed item form multiple types if there is _type field', () => {
                  var arr = Typorama.Array.of([UserType,AddressType]).create([]);
                 arr.push({_type:'User'});
                 arr.push({_type:'Address'});
@@ -160,7 +217,7 @@ describe('Array data', function() {
                     expect(concatRes.at(i)).to.equal(i+1);
                 }
             });
-            xit('should allow subtypes allowed by all the different arrays',function(){
+            it('should allow subtypes allowed by all the different arrays',function(){
                 var userList = Typorama.Array.of(UserType).create([{}]);
                 var addressList = Typorama.Array.of(AddressType).create([{}]);
                 var mixedList = [{_type:userList.displayName},{_type:addressList.displayName}];
@@ -173,7 +230,48 @@ describe('Array data', function() {
 
             });
         });
-        describe('splice()',function(){
+
+        describe('map()', function() {
+            it('calls a callback function on every item in an array and constructs a new array from the results', () => {
+
+                var numberList = Typorama.Array.of(Typorama.Number).create([1, 2, 3]);
+                var doubles = function(num) {
+                    return num * 2;
+                };
+                var newList = numberList.map(doubles);
+                // Take a callback function and return an array
+                expect(newList instanceof Typorama.Array).to.equal(true);
+                // Make sure the values and length are correct
+                expect(newList.__value__).to.eql([2, 4, 6]);
+                // Array to be invalidated
+                expect(newList.$isInvalidated()).to.equal(false);
+            });
+
+            it('passes the extra argument when callback is envoked', () => {
+                function A() {
+                    this.arr = [1, 2, 3];
+                    this.factor = 2;
+                    this.multi = function(n) {
+                        return n * this.factor;
+                    };
+                }
+
+                var a = new A(),
+                    b = new A();
+                a.doIt = function() {
+                    return this.arr.map(this.multi, this);
+                };
+                // This cannot run as this.multi actually runs on this.arr
+                // b.doIt = function(){
+                //   return this.arr.map(this.multi);
+                // };
+
+                expect(a.doIt()).to.eql([2, 4, 6]);
+                // expect(b.doIt()).to.eql([NaN, NaN, NaN]);
+            });
+        });
+        describe('splice()',function() {
+
             it('changes the content of an array by removing existing elements and/or adding new elements', () => {
                 var numberList = Typorama.Array.of(Typorama.Number).create([1,2,3,4]);
                 var removedItems = numberList.splice(1,2,7,10,13);
@@ -297,7 +395,6 @@ describe('Array data', function() {
             });
         });
 
-
         describe('as field on data object', () => {
 
             var GroupType = Typorama.define('GroupType', {
@@ -398,7 +495,6 @@ describe('Array data', function() {
                 expect(newIndex).to.equal(null);
                 expect(numberList.length).to.equal(lengthBeforePush);
                 expect(numberList.at(4)).to.equal(undefined);
-
             })
         });
         describe('splice()',function(){
