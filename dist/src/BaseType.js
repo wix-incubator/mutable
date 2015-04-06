626cf56fb8cc96654dc0a53ac9fab4d9534ec31c
LeonFedotov <leonf@wix.com>
2015-04-06 13:49:02 +0300

    build

--- dist/src/BaseType.js
+++ dist/src/BaseType.js
@@ -1,10 +1,10 @@
 (function (factory) {
     if (typeof define === "function" && define.amd) {
-        define(["exports", "module", "lodash"], factory);
+        define(["exports", "module", "lodash", "./lifecycle"], factory);
     } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
-        factory(exports, module, require("lodash"));
+        factory(exports, module, require("lodash"), require("./lifecycle"));
     }
-})(function (exports, module, _lodash) {
+})(function (exports, module, _lodash, _lifecycle) {
     "use strict";
 
     var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
@@ -14,6 +14,8 @@
     var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
     var _ = _interopRequire(_lodash);
+
+    var dirty = _lifecycle.dirty;
 
     function createReadOnly(source) {
         var readOnlyInstance = Object.create(source);
@@ -30,7 +32,7 @@
 
             this.__isReadOnly__ = false;
             this.__readOnlyInstance__ = createReadOnly(this);
-            this.__isInvalidated__ = -1;
+            this.__dirty__ = dirty.unKnown;
             this.__options__ = options;
             this.__value__ = this.constructor.wrapValue(value === undefined ? this.constructor.defaults() : value, this.constructor._spec, options);
         }
@@ -40,12 +42,14 @@
                 value: function setValue(newValue) {
                     var _this = this;
 
-                    this.__isInvalidated__ = true;
+                    this.$setDirty();
                     if (newValue instanceof BaseType) {
                         newValue = newValue.toJSON();
                     }
                     _.forEach(newValue, function (fieldValue, fieldName) {
-                        _this[fieldName] = fieldValue;
+                        if (_this.constructor._spec[fieldName]) {
+                            _this[fieldName] = fieldValue;
+                        }
                     });
                 }
             },
@@ -54,47 +58,41 @@
                     return this.__readOnlyInstance__;
                 }
             },
-            $isInvalidated: {
-                value: function $isInvalidated() {
-                    var _this = this;
+            $setDirty: {
 
-                    if (this.__isInvalidated__ === -1) {
-                        var invalidatedField = _.find(this.constructor._spec, function (fieldDef, fieldName) {
-                            if (fieldDef.type.prototype instanceof BaseType) {
-                                return _this.__value__[fieldName].$isInvalidated();
+                // called when a change has been made to this object directly #lifecycle
+
+                value: function $setDirty() {
+                    this.__dirty__ = dirty.yes;
+                }
+            },
+            $isDirty: {
+
+                // may be called after changes are paused #lifecycle
+
+                value: function $isDirty(cache) {
+                    var result = this.__dirty__.isKnown ? this.__dirty__.isDirty : _.any(this.__value__, function (val) {
+                        return val instanceof BaseType && val.$isDirty();
+                    });
+                    if (cache && !this.__isReadOnly__) {
+                        this.__dirty__ = result ? dirty.yes : dirty.no;
+                    }
+                    return result;
+                }
+            },
+            $resetDirty: {
+
+                // resets the dirty state to unknown #lifecycle
+
+                value: function $resetDirty() {
+                    if (!this.__isReadOnly__) {
+                        this.__dirty__ = dirty.unKnown;
+                        _.forEach(this.__value__, function (val) {
+                            if (val instanceof BaseType) {
+                                val.$resetDirty();
                             }
                         });
-                        if (invalidatedField) {
-                            this.__isInvalidated__ = true;
-                        } else {
-                            this.__isInvalidated__ = false;
-                        }
                     }
-                    return this.__isInvalidated__;
-                }
-            },
-            $revalidate: {
-                value: function $revalidate() {
-                    var _this = this;
-
-                    this.__isInvalidated__ = -1;
-                    _.forEach(this.constructor._spec, function (fieldDef, fieldName) {
-                        if (fieldDef.type.prototype instanceof BaseType) {
-                            _this.__value__[fieldName].$revalidate();
-                        }
-                    });
-                }
-            },
-            $resetValidationCheck: {
-                value: function $resetValidationCheck() {
-                    var _this = this;
-
-                    this.__isInvalidated__ = this.__isInvalidated__ || -1;
-                    _.forEach(this.constructor._spec, function (fieldDef, fieldName) {
-                        if (fieldDef.type.prototype instanceof BaseType) {
-                            _this.__value__[fieldName].$resetValidationCheck();
-                        }
-                    });
                 }
             },
             toJSON: {
