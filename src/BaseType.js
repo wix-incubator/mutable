9cea90990d27251fd5f361bbda66729ee34577ca
amir-arad <amira@wix.com>
2015-04-16 17:58:28 +0300

    separated dirtifyable behaviors from BaseType

--- src/BaseType.js
+++ src/BaseType.js
@@ -1,5 +1,5 @@
 import _ from "lodash"
-import {dirty} from "./lifecycle"
+import {dirty, makeDirtyable} from "./lifecycle"
 
 
 function createReadOnly(source){
@@ -28,7 +28,6 @@ export default class BaseType {
     constructor(value, options = {}){
         this.__isReadOnly__ = false;
         this.__readOnlyInstance__ = createReadOnly(this);
-        this.__dirty__ = dirty.unKnown;
         this.__options__ = options;
         this.__value__ = this.constructor.wrapValue(
             (value === undefined) ? this.constructor.defaults() : value,
@@ -56,34 +55,6 @@ export default class BaseType {
         return this.__readOnlyInstance__;
     }
 
-    // called when a change has been made to this object directly or after changes are paused #lifecycle
-    $setDirty(isDirty) {
-        if (!this.__isReadOnly__ && isDirty !== undefined) {
-            this.__dirty__ = isDirty ? dirty.yes : dirty.no;
-        }
-    }
-
-    // may be called at any time #lifecycle
-    $isDirty() {
-        return this.__dirty__.isKnown ? this.__dirty__.isDirty :
-            _.any(this.__value__, (val) => val.$isDirty && val.$isDirty());
-    }
-
-    // resets the dirty state to unknown #lifecycle
-    $resetDirty(){
-        if (!this.__isReadOnly__) {
-            this.__dirty__ = dirty.unKnown;
-            _.forEach(this.__value__, (val) => {
-                if (val.$resetDirty && _.isFunction(val.$resetDirty)) {
-                    val.$resetDirty();
-                }
-            });
-        } else {
-            // todo:warn hook
-            console.warn('resetting dirty flag on read only!');
-        }
-    }
-
     toJSON(){
         return Object.keys(this.constructor._spec).reduce((json, key) => {
             var fieldValue = this.__value__[key];
@@ -98,3 +69,5 @@ export default class BaseType {
     }
 
 }
+
+makeDirtyable(BaseType);
