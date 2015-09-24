b32a94e36f71a0d35794eface9604d54a32aa86e
Amir Arad <amira@wix.com>
2015-09-24 15:30:34 +0300

    prettyfying

--- src/defineType.js
+++ src/defineType.js
@@ -85,11 +85,11 @@ function generateFieldsOn(obj, fieldsDefinition) {
 
         Object.defineProperty(obj, fieldName, {
             get: function() {
-				var value = this.__value__[fieldName];
-                if (!BaseType.isAssignableFrom(fieldDef.type) || this.$isDirtyable()) {
-                    return this.__value__[fieldName];
+		var value = this.__value__[fieldName];
+                if (!BaseType.isAssignableFrom(fieldDef.type) || this.$isDirtyable() || value === null || value === undefined) {
+                    return value;
                 } else {
-                    return (value === null || value === undefined) ? value : value.$asReadOnly();
+                    return value.$asReadOnly();
                 }
             },
             set: function(newValue) {
