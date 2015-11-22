329914a652b39d7829bf05cfede0affa1b5b93eb
Ido Rosenthal <idoros@users.noreply.github.com>
2015-11-22 12:18:08 +0200

    add transitional signature
    temporary added the new signature in order to smoothly move all calls to the new signature

--- src/defineType.js
+++ src/defineType.js
@@ -6,7 +6,7 @@ import {getMailBox}       from 'gopostal';
 
 const MAILBOX = getMailBox('Typorama.define');
 
-export default function(id, typeDefinition, TypeConstructor){
+function defineType(id, typeDefinition, TypeConstructor){
 
     var Type = TypeConstructor || function Type(value, options){
         BaseType.call(this, value, options);
@@ -42,6 +42,12 @@ export default function(id, typeDefinition, TypeConstructor){
 
     return Type;
 };
+
+defineType.newImpl = function(id, typeDefinition, ParentType, TypeConstructor){
+	return defineType(id, typeDefinition, TypeConstructor);
+};
+
+export default defineType;
 
 function getComplexFields(spec){
 	var complex = [];
