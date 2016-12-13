f20777e0a87e687f06dad398703bf121e12d2c8a
amir-arad <amira@wix.com>
2016-12-13 21:18:12 +0200

    initial WIP

---
+++ src/field.js
@@ -0,0 +1,51 @@
+import {BaseAtom} from 'mobx';
+
+
+/**
+ * A Field is a typed reference to a mutable object.
+ */
+
+export class FieldBuilder{
+    spec;
+    name;
+    constructor(name, spec) {
+        this.name = name;
+        this.spec = spec;
+    }
+
+
+}
+
+export class Field{
+    spec;
+
+    constructor(name, spec) {
+        super(name);
+        this.spec = spec;
+    }
+
+    atom(instance){
+        return instance.__atoms__[name];
+    }
+
+    value(instance){
+        return instance.__value__[name];
+    }
+
+}
+
+// "inherit" type API
+[
+    '_matchValue',
+    'allowPlainVal',
+    'isNullable',
+    'create',
+    'defaults',
+    'validate',
+    'validateType',
+    'isJsAssignableFrom'
+].forEach(funcName => {
+    Field.prototype[funcName] = function () {
+        return this.fieldSpec[funcName].apply(this.fieldSpec, arguments);
+    };
+});
