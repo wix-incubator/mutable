45237b2ed54894cf51c8b45d0a9eec6a02b1918e
amir-arad <amira@wix.com>
2017-01-15 12:36:01 +0200

    add reflection and mobx reaction regression tests for user-defined classes

---
+++ test/reflection.spec.ts
@@ -0,0 +1,34 @@
+import * as mutable from '../src';
+import {expect} from 'chai';
+
+describe('user defined class', () => {
+    let UserClass:any, UserClassChild:any;
+    before(()=>{
+        UserClass = mutable.define('UserClass', {spec:(c)=>({
+            foo:mutable.Number
+        })});
+        UserClassChild = mutable.define('UserClassChild', {spec:(c)=>({
+            bar:mutable.Number
+        })}, UserClass);
+    });
+    it('is any', () => {
+        expect(mutable.isAny(UserClass), 'UserClass').to.be.true;
+        expect(mutable.isAny(UserClassChild), 'UserClassChild').to.be.true;
+    });
+    it('is mutable', () => {
+        expect(mutable.isMutable(UserClass), 'UserClass').to.be.true;
+        expect(mutable.isMutable(UserClassChild), 'UserClassChild').to.be.true;
+    });
+    it('is class', () => {
+        expect(mutable.isClass(UserClass), 'UserClass').to.be.true;
+        expect(mutable.isClass(UserClassChild), 'UserClassChild').to.be.true;
+    });
+    it('is not enum', () => {
+        expect(mutable.isEnum(UserClass), 'UserClass').to.be.false;
+        expect(mutable.isEnum(UserClassChild), 'UserClassChild').to.be.false;
+    });
+    it('is not nullable', () => {
+        expect(mutable.isNullable(UserClass), 'UserClass').to.be.false;
+        expect(mutable.isNullable(UserClassChild), 'UserClassChild').to.be.false;
+    });
+});
