b3c15bbd07e67ffdc10bd9cfe8c5820df860bee7
amir-arad <amira@wix.com>
2017-01-15 15:55:19 +0200

    fix typo in global module solution

--- src/singleton-module.ts
+++ src/singleton-module.ts
@@ -4,7 +4,7 @@ const globalCtx = (typeof self === 'object' && self.self === self && self) ||
     (typeof global === 'object' && global['global'] === global && global) ||
     this;
 export function setGlobalModule(globalModule:any){
-    if (!globalCtx.__Mutabl) {
+    if (!globalCtx.__Mutable) {
         globalCtx.__Mutable = globalModule;
     }
     return globalCtx.__Mutable;
