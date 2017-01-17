f45059927c6f26da88c04a4578db949793171e77
amir-arad <amira@wix.com>
2017-01-17 10:17:02 +0200

    Merge branch 'master' of github.com:wix/mutable into custom-mobx-classes
    # Conflicts:
    #	src/objects/default-object.ts
    #	src/objects/define.ts
    #	src/objects/object.ts
    #	src/objects/reference.ts

--- src/objects/default-object.ts
+++ src/objects/default-object.ts
@@ -5,7 +5,7 @@ let DefaultClass: Class<{}|null>|null = null;
 export function defaultObject(value:any){
     if (!DefaultClass){
         // lazy class definition because `defineClass` is a circular reference
-        DefaultClass = defineClass('Default', { spec: () => ({}) }).nullable();
+        DefaultClass = defineClass('Default', { spec: () => ({}) }).nullable() as Class<{}|null>;
     }
     return DefaultClass.create(value);
 }
